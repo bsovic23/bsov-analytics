0235d57048ad57aa1f00fde41e95776705329c9a
Brit Sovic <Brit.Sovic@kidney.org>
2024-05-10 16:53:09 -0400

    updating ce typescript interface

---
+++ client/src/functions/ceFx.ts
@@ -0,0 +1,8 @@
+// Fx 1 (slide 7): Learner Course Enrollments, Learner Course Completions 
+// Fx :
+// Fx (slide 13): Course Enrollments, Course Completions, New Accounts
+// Fx (slide 14): Enrollments by month
+// Fx (slide 15): Enrollments by discipline (nursem dietician, physician... etc)
+// Fx (slide 16): Top 10 Enrolled Programs (course title, enrollment amounts)
+// Fx (slide 17): Top 10 Completed Programs (Course Title, amount completed)
+
